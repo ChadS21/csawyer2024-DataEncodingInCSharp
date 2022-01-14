ceb207a09827e309bbe4d54d598d11065130a887
ChadS21 <csawyer2024@chadwickschool.org>
2022-01-14 08:37:29 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csawyer2024_DataEncodingInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
